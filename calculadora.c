15694fcf2c8d70b2056e706e74d36b83a227ae1f
m183 <39632933+m183@users.noreply.github.com>
2022-09-07 13:04:50 -0500

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -6,15 +6,21 @@ void main(void){
 int n1,n2,i,j;
 
 printf("BIENVENIDO A LA CALCULADORA");
-  printf("Mi chavo el Hernando")
+  printf("Mi chavo el Hernando");
   printf("Introduce un número Para seleccionar una opción del menú:");
   printf("1.-Suma");
   printf("2.-Multiplicación");
+  printf("3.-Resta");
+  printf("4.-Division");
   scanf("%d",n1);
+  
+  printf("Ingrese un numero")
+    printf("El resultado es ")
+    printf("Fin")
+  
 
 
-  resta=a-b
-    printf("El resultado de la Resta es: %.2f\n",resta); 
+
 
 
 }
