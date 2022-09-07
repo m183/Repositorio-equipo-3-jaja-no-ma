f77ba0b27af6e185453c8ce66c7a4c02259d0479
DonRojo90 <93852720+DonRojo90@users.noreply.github.com>
2022-09-07 13:01:53 -0500

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -6,11 +6,15 @@ void main(void){
 int n1,n2,i,j;
 
 printf("BIENVENIDO A LA CALCULADORA");
-  printf("Mi chavo el Hernando")
+  printf("Mi chavo el Hernando");
   printf("Introduce un número Para seleccionar una opción del menú:");
   printf("1.-Suma");
   printf("2.-Multiplicación");
+  printf("3.-Resta");
+  printf("4.-Division");
   scanf("%d",n1);
+  
+  
 
 
 
