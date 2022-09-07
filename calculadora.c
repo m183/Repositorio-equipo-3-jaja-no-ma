5645af4e4b28908093c7d4196fadec1a1761e155
biro24 <94942087+biro24@users.noreply.github.com>
2022-09-07 13:07:51 -0500

    Create calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -3,19 +3,36 @@
 
 
 void main(void){
-int n1,n2,i,j;
+int n1,n2,i,j,a,b,suma,mul;
 
 printf("BIENVENIDO A LA CALCULADORA");
-  printf("Mi chavo el Hernando")
+  printf("Mi chavo el Hernando");
   printf("Introduce un número Para seleccionar una opción del menú:");
   printf("1.-Suma");
   printf("2.-Multiplicación");
+  printf("3.-Resta");
+  printf("4.-División");
   scanf("%d",n1);
-
-
-  resta=n1-n2
-    printf("El resultado de la Resta es: %.2f\n",resta); 
   
+  Switch(n1)
+  {
+  Case1:
+    printf("Introduce el 1er número a sumar:");
+    scanf("%d",a);
+    printf("/nIntroduce el 2do número a sumar:");
+    scanf("%d",b);
+    suma=a+b;
+    printf("Tu resultado es:%d",suma);
+   Case2:
+    printf("Introduce el Multiplicando:");
+    scanf("%d",a);
+    printf("/nIntroduce el Multiplicador:");
+    scanf("%d",b);
+    mul=a*b;
+    printf("Tu resultado es:%d",mul);
+  }
+  
+
 
 
 }
